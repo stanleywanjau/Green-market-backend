a497330e885a0aba7b27398110eefc47d76be9dd
stanley <stanleywanjau371@gmail.com>
2024-03-02 23:58:47 +0300

    Add tables for better organization and structure

---
+++ server/config.py
@@ -0,0 +1,19 @@
+from flask import Flask 
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_restful import Api
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity,unset_jwt_cookies, jwt_required, JWTManager
+
+db = SQLAlchemy()
+app = Flask(__name__)
+app.secret_key=b'\xae\xf15\xb5\xfa\x8b\xafz%%\x19\xe8\xb4\xc5\x06\x8f'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Greenmarket.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+migrate=Migrate(app,db)
+db.init_app(app)
+
+bcrypt=Bcrypt(app)
+api = Api(app)
